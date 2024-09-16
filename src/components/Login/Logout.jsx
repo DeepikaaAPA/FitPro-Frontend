4c76f64e9ad401c82a062fb2fba47c16005838e6
DeepikaaAPA <deepikaarjun@gmail.com>
2024-09-16 11:26:01 +0530

    add logout page

--- src/components/Login/Logout.jsx
+++ src/components/Login/Logout.jsx
@@ -1,3 +1,34 @@
-export default function Logout() {
-  return <>Logout</>;
-}
+import { useEffect } from "react";
+
+import { useNavigate } from "react-router-dom";
+import instance from "../../services/instance";
+
+const Logout = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // perform the logout
+    instance
+      .post("/auth/logout")
+      .then((response) => {
+        alert(response.data.message);
+
+        // redirect to the login page
+        setTimeout(() => {
+          navigate("/login");
+        }, 500);
+      })
+      .catch((error) => {
+        alert(error?.response?.data?.message);
+
+        // redirect to the login page
+        setTimeout(() => {
+          navigate("/login");
+        }, 500);
+      });
+  }, []);
+
+  return <div>Logging out...</div>;
+};
+
+export default Logout;
