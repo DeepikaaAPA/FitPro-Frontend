d362f3b3438e8649b420e74b62a6aa3ee8cb8473
DeepikaaAPA <deepikaarjun@gmail.com>
2024-10-08 15:28:39 +0530

    change : config

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,5 @@
-export const baseURL = "http://localhost:3001/api/v1";
-export const FRONTEND_URL = "http://localhost:5173";
+// export const baseURL = "http://localhost:3001/api/v1";
+// export const FRONTEND_URL = "http://localhost:5173";
+
+export const baseURL = "https://fitpro-backend.onrender.com/api/v1";
+export const FRONTEND_URL = "https://fitpro-webapp.netlify.app";
